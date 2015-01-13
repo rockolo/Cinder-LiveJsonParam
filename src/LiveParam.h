a15b0184d021d5c77dcb600868f00ff65c4e5382
Eric Renaud-Houde <eric.renaud.houde@gmail.com>
2015-01-13 13:39:09 -0500

    Fixed template specialization to be cross-platform.

--- src/LiveParam.h
+++ src/LiveParam.h
@@ -80,11 +80,8 @@ namespace live {
 			}
 		}
 		
-		void save( const std::string& name, ci::JsonTree* tree ) const override
-		{
-			tree->addChild( ci::JsonTree( name, ci::toString( mValue ) ) );
-		}
-		void load( const std::string& name, ci::JsonTree::ConstIter& iter ) override { }
+		void save( const std::string& name, ci::JsonTree* tree ) const override;
+		void load( const std::string& name, ci::JsonTree::ConstIter& iter ) override;
 	
 		T						mValue;
 		std::function<void()>	mUpdateFn;
@@ -124,127 +121,5 @@ namespace live {
 		ci::fs::path	mJsonFilePath;
 	};
 
-
-	template<>
-	void Param<glm::vec2>::save( const std::string& name, ci::JsonTree* tree ) const
-	{
-		auto v = ci::JsonTree::makeArray( name );
-		v.pushBack( ci::JsonTree( "x", ci::toString( mValue.x ) ) );
-		v.pushBack( ci::JsonTree( "y", ci::toString( mValue.y ) ) );
-		tree->addChild( v );
-	}
-
-	template<>
-	void Param<glm::vec3>::save( const std::string& name, ci::JsonTree* tree ) const
-	{
-		auto v = ci::JsonTree::makeArray( name );
-		v.pushBack( ci::JsonTree( "x", ci::toString( mValue.x ) ) );
-		v.pushBack( ci::JsonTree( "y", ci::toString( mValue.y ) ) );
-		v.pushBack( ci::JsonTree( "z", ci::toString( mValue.z ) ) );
-		tree->addChild( v );
-	}
-
-	template<>
-	void Param<glm::vec4>::save( const std::string& name, ci::JsonTree* tree ) const
-	{
-		auto v = ci::JsonTree::makeArray( name );
-		v.pushBack( ci::JsonTree( "x", ci::toString( mValue.x ) ) );
-		v.pushBack( ci::JsonTree( "y", ci::toString( mValue.y ) ) );
-		v.pushBack( ci::JsonTree( "z", ci::toString( mValue.z ) ) );
-		v.pushBack( ci::JsonTree( "w", ci::toString( mValue.w ) ) );
-		tree->addChild( v );
-	}
-
-	template<>
-	void Param<glm::quat>::save( const std::string& name, ci::JsonTree* tree ) const
-	{
-		auto v = ci::JsonTree::makeArray( name );
-		v.pushBack( ci::JsonTree( "w", ci::toString( mValue.w ) ) );
-		v.pushBack( ci::JsonTree( "x", ci::toString( mValue.x ) ) );
-		v.pushBack( ci::JsonTree( "y", ci::toString( mValue.y ) ) );
-		v.pushBack( ci::JsonTree( "z", ci::toString( mValue.z ) ) );
-		tree->addChild( v );
-
-	}
-
-	template<>
-	void Param<ci::Color>::save( const std::string& name, ci::JsonTree* tree ) const
-	{
-		auto v = ci::JsonTree::makeArray( name );
-		v.pushBack( ci::JsonTree( "r", ci::toString( mValue.r ) ) );
-		v.pushBack( ci::JsonTree( "g", ci::toString( mValue.g ) ) );
-		v.pushBack( ci::JsonTree( "b", ci::toString( mValue.b ) ) );
-		tree->addChild( v );
-	}
-
-	template<>
-	void Param<bool>::load( const std::string& name, ci::JsonTree::ConstIter& iter )
-	{
-		update( iter->getValue<bool>() );
-	}
-
-	template<>
-	void Param<int>::load( const std::string& name, ci::JsonTree::ConstIter& iter )
-	{
-		update( iter->getValue<int>() );
-	}
-
-	template<>
-	void Param<float>::load( const std::string& name, ci::JsonTree::ConstIter& iter )
-	{
-		update( iter->getValue<float>() );
-	}
-
-	template<>
-	void Param<glm::vec2>::load( const std::string& name, ci::JsonTree::ConstIter& iter )
-	{
-		glm::vec2 v;
-		v.x = iter->getChild( "x" ).getValue<float>();
-		v.y = iter->getChild( "y" ).getValue<float>();
-		update( v );
-	}
-
-	template<>
-	void Param<glm::vec3>::load( const std::string& name, ci::JsonTree::ConstIter& iter )
-	{
-		glm::vec3 v;
-		v.x = iter->getChild( "x" ).getValue<float>();
-		v.y = iter->getChild( "y" ).getValue<float>();
-		v.z = iter->getChild( "z" ).getValue<float>();
-		update( v );
-	}
-
-	template<>
-	void Param<glm::vec4>::load( const std::string& name, ci::JsonTree::ConstIter& iter )
-	{
-		glm::vec4 v;
-		v.x = iter->getChild( "x" ).getValue<float>();
-		v.y = iter->getChild( "y" ).getValue<float>();
-		v.z = iter->getChild( "z" ).getValue<float>();
-		v.w = iter->getChild( "w" ).getValue<float>();
-		update( v );
-	}
-
-	template<>
-	void Param<glm::quat>::load( const std::string& name, ci::JsonTree::ConstIter& iter )
-	{
-		glm::quat q;
-		q.w = iter->getChild( "w" ).getValue<float>();
-		q.x = iter->getChild( "x" ).getValue<float>();
-		q.y = iter->getChild( "y" ).getValue<float>();
-		q.z = iter->getChild( "z" ).getValue<float>();
-		update( q );
-	}
-
-	template<>
-	void Param<ci::Color>::load( const std::string& name, ci::JsonTree::ConstIter& iter )
-	{
-		ci::Color c;
-		c.r = iter->getChild( "r" ).getValue<float>();
-		c.g = iter->getChild( "g" ).getValue<float>();
-		c.b = iter->getChild( "b" ).getValue<float>();
-		update( c );
-	}
-	
 } //namespace live
 
